0fb3102347473757d57386a4d840a5af9221658f
goudamy <goudamy.s@gmail.com>
2015-12-12 21:24:39 -0800

    Update ListEvent.java

--- SkiBuddy/app/src/main/java/com/cmpe277/skibuddy/ListEvent.java
+++ SkiBuddy/app/src/main/java/com/cmpe277/skibuddy/ListEvent.java
@@ -3,122 +3,288 @@ package com.cmpe277.skibuddy;
 /**
  * Created by goudamy on 10/3/2015.
  */
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
+
 public class ListEvent extends Fragment {
 
-    //To Do: use APIs to call all the details of the event and fill in the following array structure
-    String[] name = {"Tahoe Ski 2015", "Snowboarding Competition", "Yosemite Skiers Race", "Mount Everest Race"};
-    String[] start_date = {"10-20-2015", "10-21-2015", "10-22-2015", "10-23-2015"};
-    String[] end_date = {"10-20-2015", "10-21-2015", "10-22-2015", "10-23-2015"};
-    String[] start_time = {"10:20", "10:21", "10:22", "10:23"};
-    String[] end_time = {"11:20", "11:21", "11:22", "12:23"};
-    String[] eventId = {"1", "2", "3", "4"};
-    String[] title = {"Tahoe champioship skiers", "Sierra snowboarders", "Yosemite Regionals Race", "Can you make it?"};
-    //Here join variable is used to find if the user has joined the event or not
-    boolean[] join = {true, false, true, false};
-    ListView listview;
-    String[] venue = {"Tahoe", "Mount Hood", "Yosemite", "Himalayas"};
+    String userName;
+    public ListEvent()
+    {
 
+    }
+    @SuppressLint("ValidFragment")
+     ListEvent(String user){
+     this. userName = user;
+}
+
+    //To Do: use APIs to call all the details of the event and fill in the following array structure
+    ListView listview;
+
+    ArrayList<HashMap<String,String>> list;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View search = inflater.inflate(R.layout.search_results, container, false);
         Resources res = getResources();
         listview = (ListView) search.findViewById(R.id.listView);
-        eventDetails img = new eventDetails(getContext(), name, start_date, end_date, start_time, end_time, venue, join, eventId, title);
-        listview.setAdapter(img);
 
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Toast.makeText(getContext(), "Click ListItem Number " + position, Toast.LENGTH_LONG)
-                        .show();
-            }
-        });
+
+        String entire = "'user_id'" + ":'" + userName + "'";
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        final Fragment frag = this;
+
+            try {
+                StringEntity entity = new StringEntity("{'data': [{"+ entire+"}]}");
+
+                client.post(getContext(), "http://52.90.230.67:8000/getEvent/", entity, "application/json",
+                        new AsyncHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            try {
+
+                                JSONObject arr = new JSONObject(new String(responseBody));
+                                JSONObject jsonObject = arr.getJSONObject("data");
+
+                                JSONArray jArray = jsonObject.getJSONArray("eventId");
+                                String[] eventId = jArray.toString().replace("},{", " ,").replace("[","").replace("]","").split(",");
+
+
+                                jArray = jsonObject.getJSONArray("join");
+                                String[] join = jArray.toString().replace("},{", " ,").replace("[","").replace("]","").replace("]","").split(",");
+
+                                jArray = jsonObject.getJSONArray("end_time");
+                                String[] end_time = jArray.toString().replace("},{", " ,").replace("[","").replace("]","").split(",");
+
+
+                                jArray = jsonObject.getJSONArray("start_time");
+                                String[] start_time = jArray.toString().replace("},{", " ,").replace("[","").replace("]","").split(",");
+
+                                jArray = jsonObject.getJSONArray("venue");
+                                String[] venue = jArray.toString().replace("},{", " ,").replace("[","").replace("]","").split(",");
+
+                                jArray = jsonObject.getJSONArray("title");
+                                String[] title = jArray.toString().replace("},{", " ,").replace("[","").replace("]","").split(",");
+
+                                jArray = jsonObject.getJSONArray("description");
+                                String[] name = jArray.toString().replace("},{", " ,").replace("[","").replace("]","").split(",");
+
+                                list = new ArrayList<HashMap<String,String>>();
+
+                                for(int i=0;i<eventId.length;i++) {
+                                    HashMap<String,String> temp = new HashMap<String,String>();
+                                    temp.put("name", name[i]);
+                                    temp.put("title", title[i]);
+                                    temp.put("venue", venue[i]);
+                                    temp.put("startTime", start_time[i]);
+                                    temp.put("endTime", end_time[i]);
+                                    temp.put("eventId",eventId[i]);
+                                    temp.put("join", join[i]);
+                                    list.add(temp);
+                                }
+                                eventDetails adapter = new eventDetails(frag,list,userName);
+                                listview.setAdapter(adapter);
+                                }
+
+
+
+                             catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                            Log.d("Failed", "On failure called");
+                        }
+                    });
+        } catch (UnsupportedEncodingException e) {
+
+        }
 
         return search;
 
     }
 
-
 }
 
-class eventDetails extends ArrayAdapter<String> {
+class eventDetails extends BaseAdapter {
+    public ArrayList<HashMap<String,String>> list;
+    Fragment fragment;
+    String userName;
     Context context;
-    String[] event_venue;
-    String[] event_name;
-    String[] event_date;
-    String[] event_endDate;
-    String[] event_time;
-    String[] event_endtime;
-    String[] eventId;
-    String[] title;
-    boolean[] join;
 
-    eventDetails(Context c, String[] name, String[] startDate, String[] endDate, String[] startTime, String[] endTime, String[] venue, boolean[] joined, String[] eventId, String[] title) {
-
-        super(c, R.layout.rowtemplate, R.id.name, name);
-        this.context = c;
-        this.event_venue = venue;
-        this.event_date = startDate;
-        this.event_endDate = endDate;
-        this.event_name = name;
-        this.event_time = startTime;
-        this.event_endtime = endTime;
-        this.join = joined;
-        this.title = title;
-        this.eventId = eventId;
-
+    public eventDetails( Fragment fragment,ArrayList<HashMap<String,String>> list,String userName) {
+        super();
+this.fragment = fragment;
+        this.list = list;
+        this.userName = userName;
     }
 
 
     @Override
+    public int getCount() {
 
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) fragment.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
         View row = inflater.inflate(R.layout.rowtemplate, parent, false);
         TextView text = (TextView) row.findViewById(R.id.name);
         TextView venue = (TextView) row.findViewById(R.id.venue);
         TextView time = (TextView) row.findViewById(R.id.time);
         TextView title1 = (TextView) row.findViewById(R.id.title);
         TextView date = (TextView) row.findViewById(R.id.date);
-        CheckBox joined = (CheckBox) row.findViewById(R.id.checkBox);
+        final CheckBox joined = (CheckBox) row.findViewById(R.id.checkBox);
         TextView id = (TextView) row.findViewById(R.id.eventid);
         Button btn = (Button) row.findViewById(R.id.friendButton);
-        text.setText(event_name[position]);
-        venue.setText(event_venue[position]);
-        date.setText(event_date[position] + " to " + event_endDate[position]);
-        time.setText(event_time[position] + " to " + event_endtime[position]);
-        id.setText(eventId[position]);
-        title1.setText(title[position]);
         View button1 = row.findViewById(R.id.friendButton);
+
+        final HashMap<String,String> map = list.get(position);
+
+
+        text.setText( map.get("name").replace("\"", ""));
+
+        venue.setText(map.get("venue").replace("\"", ""));
+        String[] start = map.get("startTime").replace("\"", "").split("T");
+        String[] end = map.get("endTime").replace("\"", "").split("T");
+        date.setText(start[0]+"to"+ end[0]);
+        if(start.length > 1 && end.length > 1) {
+            time.setText(start[1] + "to" + end[1]);
+        }
+        id.setText(map.get("eventId").replace("\"", ""));
+        title1.setText(map.get("title").replace("\"", ""));
         button1.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        myClickHandler(eventId[position]);
+                        myClickHandler(map.get("eventId").replace("\"", ""));
                     }
                 }
         );
-        if (join[position] == true) {
-            joined.setChecked(true);
+
+        joined.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if (((CheckBox) joined).isChecked()) {
+                            String entire = "'user_id'" + ":'" + userName + "',"+"'event_id'" + ":" + map.get("eventId") ;
+
+                            AsyncHttpClient client = new AsyncHttpClient();
+                            RequestParams params = new RequestParams();
+                            try {
+                                StringEntity entity = new StringEntity("{'data': [{"+ entire+"}]}");
+
+                                client.post(context, "http://52.90.230.67:8000/joinEvent/", entity, "application/json",
+                                        new AsyncHttpResponseHandler() {
+                                            @Override
+                                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                                Log.d("Success", "On success called");
+
+                                            }
+
+                                            @Override
+                                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                                Log.d("Failed", "On failure called");
+                                            }
+                                        });
+                            }
+
+                            catch(UnsupportedEncodingException e) {
+
+                            }
+
+
+                        } else {
+
+                            String entire = "'user_id'" + ":'" + userName + "',"+"'event_id'" + ":" + map.get("eventId") ;
+
+                            AsyncHttpClient client = new AsyncHttpClient();
+                            RequestParams params = new RequestParams();
+                            try {
+                                StringEntity entity = new StringEntity("{'data': [{"+ entire+"}]}");
+
+                                client.post(context, "http://52.90.230.67:8000/unjoinEvent/", entity, "application/json",
+                                        new AsyncHttpResponseHandler() {
+                                            @Override
+                                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                                Log.d("Success", "On success called");
+                                            }
+
+                                            @Override
+                                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                                Log.d("Failed", "On failure called");
+                                            }
+                                        });
+                            }
+
+                            catch(UnsupportedEncodingException e) {
+
+                            }
+
+
+                        }
+
+                    }
+                }
+        );
+
+        if (map.get("join").equals("true") ){
+           joined.setChecked(true);
         } else {
             joined.setChecked(false);
         }
@@ -129,13 +295,11 @@ class eventDetails extends ArrayAdapter<String> {
     public void myClickHandler(String eventId) {
 
 
-        Intent intent = new Intent(getContext(), ListFriends.class);
+        Intent intent = new Intent(fragment.getContext(), ListFriends.class);
         intent.putExtra("Event_ID", eventId);
-        getContext().startActivity(intent);
-
-        }
+        fragment.getContext().startActivity(intent);
 
     }
 
-
+}
 
