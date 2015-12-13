54dacdc6aea349032ddd4bfd7fd53b453254f48a
goudamy <goudamy.s@gmail.com>
2015-12-12 21:31:43 -0800

    Update ListFriends.java

--- SkiBuddy/app/src/main/java/com/cmpe277/skibuddy/ListFriends.java
+++ SkiBuddy/app/src/main/java/com/cmpe277/skibuddy/ListFriends.java
@@ -1,24 +1,36 @@
 package com.cmpe277.skibuddy;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 /**
  * Created by goudamy on 11/30/2015.
  */
-public class ListFriends extends Activity {
+public class ListFriends extends AppCompatActivity {
     private ListView mainListView ;
     private ArrayAdapter<String> listAdapter ;
-    String eventId;
 
     /** Called when the activity is first created. */
     @Override
@@ -27,23 +39,36 @@ public class ListFriends extends Activity {
         setContentView(R.layout.findfriends);
 
         mainListView = (ListView) findViewById(R.id.friendListView);
+
         Intent intent = getIntent();
         String id = intent.getStringExtra("Event_ID");
-        eventId = id;
-        //Toast.makeText(getApplicationContext(), "Here we go " + id, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Here we go " + id, Toast.LENGTH_LONG)
+                .show();
         //TO DO: Use API's to include friends list
-        String[] friends = new String[] {"Anusha", "Goudamy", "Sockalingam", "Dhanu",
-                "Aastha", "Purvi", "Harsha", "Tilak"};
-        ArrayList<String> friendList = new ArrayList<String>();
-        friendList.addAll( Arrays.asList(friends) );
+
+        String entire = "'event_id'" + ":'" + id +"'";
+        String[] frnds;
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        try {
+            StringEntity entity = new StringEntity("{'data': [{"+ entire+"}]}");
+
+            client.post(getApplicationContext(), "http://52.90.230.67:8000/getEventMembers/", entity, "application/json",
+                    new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            try {
+                                JSONObject arr = new JSONObject(new String(responseBody));
+                                JSONObject jsonObject = arr.getJSONObject("data");
+                                JSONArray jArray = jsonObject.getJSONArray("events");
+                                String[] friends = jArray.toString().replace("},{", " ,").replace("[", "").replace("]", "").replace("\"", "").split(",");
+                                ArrayList<String> friendList = new ArrayList<String>();
+                                friendList.addAll( Arrays.asList(friends) );
 
 
-        listAdapter = new ArrayAdapter<String>(this, R.layout.friends,R.id.friendName, friendList);
-
-
-        mainListView.setAdapter( listAdapter );
-
-        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                listAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.friends,R.id.friendName, friendList);
+                                mainListView.setAdapter( listAdapter );
+                                mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
 
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -56,8 +81,31 @@ public class ListFriends extends Activity {
 
             }
         });
-    }
 
+                            } catch (JSONException e
+                                    )
+
+                            {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers,
+                                              byte[] responseBody, Throwable error) {
+                            Log.d("Failed", "On failure called");
+                        }
+                    });
+        }
+
+        catch(UnsupportedEncodingException e) {
+
+        }
+
+
+    }
+    
     public void startMapView(View view) {
         Log.d("Hello", "Hello");
         Intent intent = new Intent(this, MapsActivity.class);
